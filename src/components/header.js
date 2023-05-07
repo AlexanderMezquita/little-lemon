9a35bb99509b027efafb700f6dbb233336abb559
Alex <alexmezsan@gmail.com>
2023-05-07 14:36:01 -0400

    components updated

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <title>Little Lemon</title>
+      <meta name="description" content="Little Lemon reservations website" />
+      <meta name="og:title" content="Little Lemon" />
+      <meta name="og:description" content="Little Lemon res app" />
+      <meta name="og:image" content="" />
+    </header>
+  );
+}
---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img alt="litle-lemon-logo" src="" />
+        </li>
+        <li>
+          <a href="#home">home</a>
+        </li>
+        <li>
+          <a href="#link ">link</a>
+        </li>
+        <li>
+          <a href="#link">link</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
